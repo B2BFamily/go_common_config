8139b91fe97b83b68dde25277310f3847896bc69
Alex <sapalks@gmail.com>
2018-04-19 15:50:54 +0400

    добавил методы для получения конфига по пути в json'e

--- confis.go
+++ confis.go
@@ -9,21 +9,29 @@ import (
 	"strings"
 )
 
-var configArg = "config:"
+var (
+	configArg   = "config:"
+	CurrentPath = ""
+)
 
 func getConfigName() (path string, err error) {
-
-	ex, err := os.Executable()
-	if err != nil {
-		panic(err)
-	}
-	exPath := filepath.Dir(ex)
-	fmt.Println(exPath)
-
+	path = ""
+	fmt.Println(CurrentPath)
 	dirname := ""
-	dirname, err = filepath.Abs(filepath.Dir(os.Args[0]))
-	if err != nil {
-		return
+	if len(CurrentPath) != 0 {
+		dirname = CurrentPath
+	} else {
+		ex, err := os.Executable()
+		if err != nil {
+			panic(err)
+		}
+		exPath := filepath.Dir(ex)
+		fmt.Println(exPath)
+
+		dirname, err = filepath.Abs(filepath.Dir(os.Args[0]))
+		if err != nil {
+			return path, err
+		}
 	}
 
 	var mode = ""
@@ -37,6 +45,50 @@ func getConfigName() (path string, err error) {
 	return
 }
 
+func readStringFromConfigFile() (jsonStr []byte, err error) {
+
+	filename, e := getConfigName()
+	if len(filename) == 0 {
+		err = e
+		return
+	}
+
+	file, e := os.Open(filename)
+	if e != nil {
+		err = e
+		return
+	}
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(file)
+	b := buf.Bytes()
+	//читаем файл
+	file.Read(b)
+	jsonStr = b
+	return
+}
+
+func getConfigFromPath(configStr []byte, pathName string, configuration interface{}) (err error) {
+	paths := strings.Split(pathName, ".")
+	// создаем срез для параметров json объекта верхнего уровня
+	c := make(map[string]interface{})
+
+	// unmarschal JSON
+	json.Unmarshal(configStr, &c)
+	for s, value := range c {
+		if s == paths[0] {
+			jsonString, _ := json.Marshal(value)
+			if len(paths) == 1 {
+				err = json.Unmarshal(jsonString, &configuration)
+			} else {
+				getConfigFromPath(jsonString, strings.Join(paths[1:], "."), &configuration)
+			}
+			return
+		}
+	}
+	return
+}
+
+//получение текущей конфигурации для всего проекта
 func GetConfig(configuration interface{}) (err error) {
 	filename, _ := getConfigName()
 	if len(filename) == 0 {
@@ -57,33 +109,8 @@ func GetConfig(configuration interface{}) (err error) {
 	return
 }
 
+//получение конфигурации, находящейся по указанному в pathName пути
 func GetConfigPath(pathName string, configuration interface{}) (err error) {
-	filename, _ := getConfigName()
-	if len(filename) == 0 {
-		return
-	}
-
-	file, err := os.Open(filename)
-	if err != nil {
-		return
-	}
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(file)
-	b := buf.Bytes()
-	//читаем файл
-	file.Read(b)
-
-	// создаем срез для параметров json объекта верхнего уровня
-	c := make(map[string]interface{})
-
-	// unmarschal JSON
-	json.Unmarshal(b, &c)
-	for s, value := range c {
-		if s == pathName {
-			jsonString, _ := json.Marshal(value)
-			err = json.Unmarshal(jsonString, &configuration)
-			return
-		}
-	}
-	return
+	configStr, _ := readStringFromConfigFile()
+	return getConfigFromPath(configStr, pathName, &configuration)
 }
